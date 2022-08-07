06d37ca345521e7a94cc766c3601e8b6d89d18a0
Poudell17 <xmraz23@gmail.com>
2022-08-07 21:01:31 +0545

    lifecycle finished component

---
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.twoactivities;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount;
+    private EditText text_edit;
+    private TextView txtCount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCount=0;
+        txtCount= (TextView)findViewById(R.id.text_count);
+
+        text_edit=(EditText) findViewById(R.id.editText_first);
+
+        if(savedInstanceState!=null){
+            mCount= savedInstanceState.getInt("count");
+            text_edit.setText(savedInstanceState.getString("textSave"));
+            txtCount.setText(Integer.toString(mCount));
+        }
+    }
+
+
+    public void countClick(View view) {
+        mCount++;
+        if(this.txtCount != null)
+            this.txtCount.setText(Integer.toString(mCount));
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mCount !=0){
+            outState.putInt("count", mCount);
+        }
+        if (text_edit.getText()!=null){
+            outState.putString("textSave",text_edit.getText().toString());
+        }
+    }
+}
---
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -0,0 +1,26 @@
+package com.example.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+
+    public void returnReply(View view) {
+
+
+    }
+
+    }
